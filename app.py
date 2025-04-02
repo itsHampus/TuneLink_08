5874bbf56184a0082d593f5ceb0203cbb578c5e8
Tilly-py <alex.s.tilly@outlook.com>
2025-04-02 14:29:42 +0200

    test2
    La till black och isort för auto-linting

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template, request, redirect, session, url_for
-from spotipy import Spotify
-from spotipy.oauth2 import SpotifyOAuth
-from spotipy.cache_handler import FlaskSessionCacheHandler
-from dotenv import load_dotenv
 import os
+
+from dotenv import load_dotenv
+from flask import Flask, redirect, render_template, request, session, url_for
+from spotipy import Spotify
+from spotipy.cache_handler import FlaskSessionCacheHandler
+from spotipy.oauth2 import SpotifyOAuth
 
 load_dotenv()
 
@@ -12,7 +13,8 @@ app.secret_key = os.getenv("FLASK_SECRET")
 
 scope = os.getenv("SPOTIPY_SCOPE")
 
-@app.route('/')
+
+@app.route("/")
 def index():
     auth_manager = SpotifyOAuth(
         client_id=os.getenv("SPOTIPY_CLIENT_ID"),
@@ -20,13 +22,14 @@ def index():
         redirect_uri=os.getenv("SPOTIPY_REDIRECT_URI"),
         scope=scope,
         cache_handler=FlaskSessionCacheHandler(session),
-        show_dialog=True
+        show_dialog=True,
     )
     auth_url = auth_manager.get_authorize_url()
-    return render_template('index.html', auth_url=auth_url)
-    return render_template('index.html')
+    return render_template("index.html", auth_url=auth_url)
+    return render_template("index.html")
 
-@app.route('/callback')
+
+@app.route("/callback")
 def callback():
     auth_manager = SpotifyOAuth(
         client_id=os.getenv("SPOTIPY_CLIENT_ID"),
@@ -34,14 +37,15 @@ def callback():
         redirect_uri=os.getenv("SPOTIPY_REDIRECT_URI"),
         scope=scope,
         cache_handler=FlaskSessionCacheHandler(session),
-        show_dialog=True
+        show_dialog=True,
     )
     code = request.args.get("code")
     token_info = auth_manager.get_access_token(code)
     session["token_info"] = token_info
-    return redirect(url_for('profile'))
+    return redirect(url_for("profile"))
 
-@app.route('/profile')
+
+@app.route("/profile")
 def profile():
     token_info = session.get("token_info")
     if not token_info:
@@ -51,23 +55,40 @@ def profile():
 
     user = sp.current_user()
 
-    top_tracks_raw = sp.current_user_top_tracks(limit=5, time_range='short_term')["items"]
-    top_tracks = [{"name": t["name"], "artists": t["artists"][0]}for t in top_tracks_raw]
+    top_tracks_raw = sp.current_user_top_tracks(limit=5, time_range="short_term")[
+        "items"
+    ]
+    top_tracks = [
+        {"name": t["name"], "artists": t["artists"][0]} for t in top_tracks_raw
+    ]
 
-    top_artists_raw = sp.current_user_top_artists(limit=5, time_range='short_term')["items"]
-    top_artists = [{"name": a["name"], "genres": a["genres"][0]if a["genres"] else "unknown"}for a in top_artists_raw]
+    top_artists_raw = sp.current_user_top_artists(limit=5, time_range="short_term")[
+        "items"
+    ]
+    top_artists = [
+        {"name": a["name"], "genres": a["genres"][0] if a["genres"] else "unknown"}
+        for a in top_artists_raw
+    ]
 
     genre_list = []
     for artists in top_artists_raw:
         genre_list.extend(artists["genres"])
     top_genres = sorted(set(genre_list))[:5]
 
-    return render_template("profile.html", user=user,top_tracks=top_tracks,top_artists=top_artists,genres=top_genres)
+    return render_template(
+        "profile.html",
+        user=user,
+        top_tracks=top_tracks,
+        top_artists=top_artists,
+        genres=top_genres,
+    )
 
-@app.route('/logout')
+
+@app.route("/logout")
 def logout():
     session.clear()
-    return redirect(url_for('index'))
+    return redirect(url_for("index"))
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
