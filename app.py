1464c804ce4030d4e2c3e22f24563ce10fc5692b
Kev-mi <90063807+Kev-mi@users.noreply.github.com>
2025-05-06 22:44:30 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ from dotenv import load_dotenv
 from flask import Flask, redirect, render_template, request, session, url_for, jsonify, flash
 
 from auth import handle_callback, spotify_auth
-from db import create_subforum_in_db, get_subforum_data, update_user_bio,search_subforums_by_name,  subscribe_to_forum, unsubscribe_from_forum, get_user_subscriptions,get_user_profile_db,get_subforum_by_name, get_all_forums
-
+from db import create_subforum_in_db, get_subforum_data, update_user_bio,search_subforums_by_name,  subscribe_to_forum, unsubscribe_from_forum, get_user_subscriptions,get_user_profile_db,get_subforum_by_name, get_all_forums, get_forum_name_by_id, create_thread
+import db
 from spotify import get_user, get_user_profile
 from spotipy import Spotify
 
@@ -272,9 +272,11 @@ def create_thread_from_subforum(forum_id):
     description = request.form.get('description')
     user_id = session['user_id']
 
-    db.create_thread(title=title, spotify_url=spotify_url, description=description, user_id=user_id, forum_id=forum_id)
+    db.create_thread(title=title, spotify_url=spotify_url, description=description, creator_id=user_id, forum_id=forum_id)
 
-    return redirect(url_for('forum_view', forum_id=forum_id))
+    forum_name = get_forum_name_by_id(forum_id)
+    return redirect(url_for('show_subforum', name=forum_name))
+
 
 
 @app.route("/thread/<int:thread_id>")
